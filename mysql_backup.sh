73593446586fd7c8e8a1c5ee45daccf4046f53c2
半醒 <469089260@163.com>
2014-01-12 17:01:19 +0800

    Update mysql_backup.sh

--- mysql_backup.sh
+++ mysql_backup.sh
@@ -2,4 +2,4 @@
 cd /home/cooiky/backup
 now=$(date +"%Y-%m-%d_%H:%M:%S")
 file=cooiky_databasename_$now.sql.gz
-mysqldump -u databaseusername -pdatabasepassword databasename | gzip > $file //-p与密码间无空格
+mysqldump -u databaseusername -pdatabasepassword databasename | gzip > $file #-p与密码间无空格
