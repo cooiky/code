1137adc2f6f1f64608b85fc2620598699a961568
半醒 <469089260@163.com>
2014-01-12 17:00:48 +0800

    Create mysql_backup.sh

---
+++ mysql_backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd /home/cooiky/backup
+now=$(date +"%Y-%m-%d_%H:%M:%S")
+file=cooiky_databasename_$now.sql.gz
+mysqldump -u databaseusername -pdatabasepassword databasename | gzip > $file //-p与密码间无空格
